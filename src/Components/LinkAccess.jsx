a88a53ded6ba9fd16fa84608d3b288b9ae058bec
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-12-16 09:59:10 -0300

    Corrige bug de estilização com a utilização de styles components

--- src/Components/LinkAccess.jsx
+++ src/Components/LinkAccess.jsx
@@ -1,19 +1,37 @@
 import React from 'react';
-import '../Styles/LinkButton.css';
+import styled from 'styled-components';
 
 function LinkAccess(props) {
   const { readyURL, title } = props;
 
-  const link = <a
-    className="accessIconButton"
-    href={readyURL}
-    target="_blank"
-    rel="noreferrer"
-  >
-    {`Acessar ícone de ${title}`}
-  </a>
+  const Button = styled.button`
+    display: block;
+    color: aliceblue;
+    text-align: center;
+    padding: 10px;
+    max-width: 50%;
+    max-height: 25px;
+    background-color: #4CAF50;;
+    border-radius: 5px;
+    margin: 10px auto;
+    text-decoration: none;
+    height: 50px;
 
-  return (link)
+    &:hover {
+      background-color: #3e8e41;
+    }
+  `;
+
+  return (
+    <Button
+      as="a"
+      href={readyURL}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      {`Acessar ícone de ${title}`}
+    </Button>
+    );
 }
 
 export default LinkAccess;
