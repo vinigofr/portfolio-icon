dac2f5e08f19b028a1337e03a578a8dafa77ccc5
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-12-16 10:12:15 -0300

    Adiciona styles componets ao componente de aviso

--- src/Components/AccessLink.jsx
+++ src/Components/AccessLink.jsx
@@ -1,18 +1,30 @@
 import React from 'react';
 import AppContext from '../ContextAPI/AppContext';
 import generateLink from '../Utils/generateLink';
-import '../Styles/IconMessage.css';
+// import '../Styles/IconMessage.css';
 import LinkAccess from './LinkAccess';
+import styled from 'styled-components';
 
 function AccessLink() {
   const { params, firstTime } = React.useContext(AppContext);
   const readyURL = generateLink(params);
 
-  const message = <p className='iconMessage'>Link de ícone ainda não gerado</p>;
-
+  const Message = styled.p`
+    height: 50px;
+    color: aliceblue;
+    text-align: center;
+    padding: 10px;
+    max-height: 25px;
+    max-width: 50%;
+    margin: 10px auto;
+    background-color: #4CAF50;
+    border-radius: 5px;
+    cursor: not-allowed;
+  `;
+  
   return (
     firstTime ?
-      message :
+      <Message>Link de ícone ainda não gerado</Message>:
       <LinkAccess readyURL={readyURL} title={params.title} />
   )
 }
