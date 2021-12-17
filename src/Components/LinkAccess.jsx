5d747af58a8157c619580c60be054554956800d8
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-12-17 14:59:09 -0300

    Adiciona função de clipboard

--- src/Components/LinkAccess.jsx
+++ src/Components/LinkAccess.jsx
@@ -3,33 +3,50 @@ import styled from 'styled-components';
 
 function LinkAccess(props) {
   const { readyURL, title } = props;
+  const [copy, setCopy] = React.useState(false);
 
   const Button = styled.button`
     display: block;
     color: aliceblue;
+    ${ copy ? 'font-weight: bold;' : '' }
     text-align: center;
-    padding: 10px;
     max-width: 50%;
-    max-height: 25px;
+    width: 100%;
     background-color: #4CAF50;;
     border-radius: 5px;
     margin: 10px auto;
     text-decoration: none;
     height: 50px;
-
+    border-style: none;
     &:hover {
       background-color: #3e8e41;
+      cursor: pointer;
+    }
+
+    &:active {
+      transform: scale(0.9);
     }
   `;
-  
+
+  // https://stackoverflow.com/questions/39501289/in-reactjs-how-to-copy-text-to-clipboard
+  const copyToClipBoard  = () => {
+    navigator.clipboard.writeText(readyURL);
+    setCopy(true);
+    setTimeout(() => {
+      setCopy(false);
+    }, 3000);
+  };
+
   return (
     <Button
-      as="a"
-      href={readyURL}
+      // as="a"
+      // href={readyURL}
       target="_blank"
       rel="noopener noreferrer"
+      data-clipboard-text={readyURL}
+      onClick={ () => copyToClipBoard() }
     >
-      {`Acessar ícone de ${title}`}
+      { copy ? 'Copiado!' : `Copiar link de ${title}` }
     </Button>
     );
 }
